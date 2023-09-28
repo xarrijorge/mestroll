af2b30e10f7bc76e43fe754a0e9857b2335a4081
Xarri Jorge <kodinglife@gmail.com>
2023-09-28 12:21:02 +0000

    Added alias to router

---
+++ src/components/charts/LineChartView.vue
@@ -0,0 +1,31 @@
+<template>
+    <Line
+      id="my-chart-id"
+      :options="chartOptions"
+      :data="chartData"
+    />
+  </template>
+  
+  <script>
+  import { Line } from 'vue-chartjs'
+  import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
+  
+  ChartJS.register( CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
+  
+  export default {
+    name: 'LineChartView',
+    // eslint-disable-next-line vue/no-reserved-component-names
+    components: { Line },
+    data() {
+      return {
+        chartData: {
+          labels: [ 'January', 'February', 'March', "April", "May", "June", "July", "August" ],
+          datasets: [ { data: [50, 48, 52, 40, 45, 32, 50, 50,] } ]
+        },
+        chartOptions: {
+          responsive: true
+        }
+      }
+    }
+  }
+  </script>
--- src/views/AboutView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h1>
-        Hey there you're uing mestroll!
-    </h1>
-</template>

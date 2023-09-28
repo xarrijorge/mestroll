c2882df6169a62025fddf473f69111409a8f07ff
Xarri Jorge <kodinglife@gmail.com>
2023-09-28 10:16:54 +0000

    dashboard setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/FellowsLoginPage.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
     {
       path: '/login',
       name: 'login',
@@ -11,11 +23,12 @@ const router = createRouter({
 
     },
     {
-      path: '/checkin',
-      name: 'checkin',
-      component: () => import('../views/CheckInView.vue')
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/DashboardView.vue')
 
     }
+
   ]
 })
 
